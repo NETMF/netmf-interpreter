277a2683fc0c8b3ce5d09b80bac62d10a1dfaa6a
Morteza Ghandehari <morteza.ghandehari@microsoft.com>
2015-06-03 16:20:07 -0700

    Delete old lwip stack

--- DeviceCode/pal/lwip/lwip/src/include/arch/bpstruct.h
+++
@@ -1,8 +0,0 @@
-#if defined(__RENESAS__)
-#pragma pack 1
-#undef PACK_STRUCT_BEGIN
-#define PACK_STRUCT_BEGIN
-#undef PACK_STRUCT_END
-#define PACK_STRUCT_END
-
-#endif
--- DeviceCode/pal/lwip/lwip/src/include/arch/epstruct.h
+++
@@ -1,5 +0,0 @@
-#if defined(__RENESAS__)
-#pragma unpack 
-#undef PACK_STRUCT_END
-#define PACK_STRUCT_END
-#endif
