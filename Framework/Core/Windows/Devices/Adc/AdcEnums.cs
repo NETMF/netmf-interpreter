fc507dad54a56540b5070bf9a24ef2104b85a34b
ColinA-MSFT <colina@microsoft.com>
2015-06-09 15:31:08 -0700

    Add support for UWP Analog APIs. Note that this currently omits a reference implementation for IAdcControllerProvider.

---
+++ Framework/Core/Windows/Devices/Adc/AdcEnums.cs
@@ -0,0 +1,17 @@
+namespace Windows.Devices.Adc
+{
+    public enum AdcChannelMode
+    {
+        SingleEnded = 0,
+        Differential,
+    }
+}
+
+namespace Windows.Devices.Adc.Provider
+{
+    public enum ProviderAdcChannelMode
+    {
+        SingleEnded = 0,
+        Differential,
+    }
+}
