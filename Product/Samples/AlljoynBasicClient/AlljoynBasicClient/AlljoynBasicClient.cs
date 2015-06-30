f858cc5dc830f4fe1808dd0f5aac53b7ff2e7001
Paul Provenza <v-joprov@microsoft.com>
2015-06-30 18:42:38 -0500

    Added some basic samples

---
+++ Product/Samples/AlljoynBasicClient/AlljoynBasicClient/AlljoynBasicClient.cs
@@ -0,0 +1,145 @@
+using System;
+using Microsoft.NETMF.Networking;
+using Microsoft.SPOT;
+using Microsoft.SPOT.AllJoyn;
+
+namespace sample_client
+{
+    public class AlljoynBasicClient
+    {
+        static uint CONNECT_TIMEOUT   = (1000 * 7);
+        static uint UNMARSHAL_TIMEOUT = (1000 * 5);
+        static uint METHOD_TIMEOUT    = (100 * 10);
+
+        static string ServiceName = "org.alljoyn.Bus.sample";
+        static string ServicePath = "/sample";
+        static UInt16 ServicePort = 25;
+        static string fullServiceName = "test";
+
+        static string[] SampleInterface = {
+            "org.alljoyn.Bus.sample",           // The first entry is the interface name
+            "?Dummy foo<i",                     // This is just a dummy entry at index 0 for illustration purposes
+            "?Dummy2 fee<i",                    // This is just a dummy entry at index 1 for illustration purposes
+            "?cat inStr1<s inStr2<s outStr>s",  // Method at index 2
+            ""
+        };
+
+        static readonly UInt32 BASIC_CLIENT_CAT = AJ.PrxMessageId(0,0,2);
+        static readonly UInt32 AJ_SIGNAL_SESSION_LOST_WITH_REASON = AJ.BusMessageId(1, 0, 17);
+
+        static UInt32 AJ_REPLY_ID(UInt32 id)
+        {
+            UInt32 rep = (id | ((UInt32)0x80 << 24));
+            return rep;
+        }
+
+        static public void MakeMethodCall(UInt32 bus, UInt32 sessionId, AJ myAlljoyn)
+        {
+            AJ_Status status = AJ_Status.AJ_OK;
+            AJ_Message msg = new AJ_Message();
+
+            status = myAlljoyn.MarshalMethodCall(bus, msg, BASIC_CLIENT_CAT, fullServiceName, sessionId, 0, METHOD_TIMEOUT);
+
+            if (status == AJ_Status.AJ_OK) {
+                status = myAlljoyn.MarshalArgs(msg, "ss", "Hello ", "World!");
+            }
+
+            if (status == AJ_Status.AJ_OK) {
+                status = myAlljoyn.DeliverMsg(msg);
+            }
+        }
+
+        public static void Main()
+        {
+            AJ_Status status = AJ_Status.AJ_OK;
+            UInt32 bus = 0;
+            bool connected = false;
+            bool done = false;
+            UInt32 sessionId = 0;
+
+            AJ myAlljoyn = new AJ();
+            myAlljoyn.CreateBus( ref bus );
+            myAlljoyn.Initialize();
+            myAlljoyn.RegisterObjectsInterface(new AJ_Object( ) { path = ServicePath, interfaces = SampleInterface }, false, false);
+
+            // Attach to Netowrk Link state change notifications
+            var netmon = new NetworkStateMonitor();
+
+
+            while (!done) {
+
+                netmon.WaitForIpAddress();
+
+                AJ_Message msg = new AJ_Message();
+
+                if (!connected) {
+
+                    status = myAlljoyn.ClientConnectBus(bus, null, CONNECT_TIMEOUT);
+
+                    status = myAlljoyn.ClientFindService(bus, ServiceName, null, CONNECT_TIMEOUT);
+
+                    status = myAlljoyn.ClientConnectService(bus, CONNECT_TIMEOUT, ServiceName, ServicePort, ref sessionId, null, ref fullServiceName);
+
+                    if (status == AJ_Status.AJ_OK) {
+                        connected = true;
+                        MakeMethodCall(bus, sessionId, myAlljoyn);
+
+                    } else {
+                        Debug.Print("Timed out");
+                        myAlljoyn.Disconnect(bus);
+                        continue;
+                    }
+                }
+
+                status = myAlljoyn.UnmarshalMsg(bus, msg, UNMARSHAL_TIMEOUT);
+
+                if (AJ_Status.AJ_ERR_TIMEOUT == status) {
+                    continue;
+                }
+
+                if (AJ_Status.AJ_OK == status) {
+                    
+                    if(msg.msgId == AJ_REPLY_ID(BASIC_CLIENT_CAT))
+                    {
+                        UInt32 argPtr = myAlljoyn.GetArgPtr(4);
+                        
+                        status = myAlljoyn.UnmarshalArg(msg, argPtr);
+
+
+                        string value = myAlljoyn.GetArgString(4);
+
+                        if (AJ_Status.AJ_OK == status) {
+
+                            Debug.Print(ServiceName + "cat returned " + value);
+
+                            done = true;
+                        } else {
+                            MakeMethodCall(bus, sessionId, myAlljoyn);
+                        }
+                    }
+                    else if (msg.msgId == AJ_SIGNAL_SESSION_LOST_WITH_REASON)
+                    {
+                        {
+                            UInt32 id=0, reason=0;
+                            myAlljoyn.UnmarshalArgs(msg, "uu", ref id, ref reason);
+                        }
+                        status = AJ_Status.AJ_ERR_SESSION_LOST;
+                    }
+                    else
+                    {
+                        /* Pass to the built-in handlers. */
+                        status = myAlljoyn.BusHandleBusMessage(msg, bus, ServicePort);
+                    }
+                }
+
+                /* Messages MUST be discarded to free resources. */
+                myAlljoyn.CloseMsg(msg);
+
+                if (status == AJ_Status.AJ_ERR_SESSION_LOST) {
+                    myAlljoyn.Disconnect(bus);
+                    //exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ Product/Samples/AlljoynBasicService/AlljoynBasicService/AlljoynBasicService.cs
@@ -0,0 +1,178 @@
+//
+// AlljoynService.cs
+//
+// Sample alljoyn service demonstrating Methods, Properties, Notifications, and About Service
+//
+//
+
+using System;
+using Microsoft.SPOT.AllJoyn;
+
+namespace AlljoynBasicService
+{
+    public class Program
+    {
+        static string ServiceName = "org.alljoyn.Bus.sample";
+        static string ServicePath = "/sample";
+        static UInt16 ServicePort = 25;
+        static string fullServiceName = "test";
+
+        static readonly UInt32 BASIC_SERVICE_CAT = AJ.AppMessageId(0, 0, 1);
+
+        static AJ_Status AppHandleCat(AJ_Message msg, AJ myAlljoyn)
+        {
+            AJ_Message reply = new AJ_Message();
+
+            string s1 = myAlljoyn.UnmarshalArgs(msg, "s");
+            string s2 = myAlljoyn.UnmarshalArgs(msg, "s");
+            
+            myAlljoyn.MarshalReplyMsg(msg, reply);
+
+            // We have the arguments. Now do the concatenation.
+            string buffer = s1 + s2;
+            
+            myAlljoyn.MarshalArg(reply, "s", buffer);
+
+            return myAlljoyn.DeliverMsg(reply);
+        }
+
+        // program entry point
+
+        public static void Main()
+        {
+            // Initialize Alljoyn class instance
+
+            AJ myAlljoyn = new AJ();
+
+            // some state variables
+
+            bool connected = false;
+            AJ_Status status = AJ_Status.AJ_OK;
+
+            // constants used in the message processing loop
+
+            UInt32 CONNECT_TIMEOUT = (1000 * 1000);
+            UInt32 UNMARSHAL_TIMEOUT = 5000;
+
+            UInt32 AJ_METHOD_ACCEPT_SESSION = AJ.BusMessageId(2, 0, 0);
+            UInt32 AJ_SIGNAL_SESSION_JOINED = AJ.BusMessageId(2, 0, 1);
+
+            byte AJ_FLAG_GLOBAL_BROADCAST = 0x20;
+
+            UInt16 port = 0;
+            UInt32 bus = 0;
+            AJ_Message msg = new AJ_Message();            
+
+
+            // Define the Alljoyn interface that will be
+            // published to the outside world
+
+            string[] testInterface = {
+                "org.alljoyn.Bus.sample",           // The first entry is the interface name. 
+                "?Dummy foo<i",                     // This is just a dummy entry at index 0 for illustration purposes. 
+                "?cat inStr1<s inStr2<s outStr>s",  // Method at index 1. 
+                " "
+            };
+           
+            // Create the Alljoyn bus instance
+
+            myAlljoyn.CreateBus(ref bus);
+
+            // Initialize Alljoyn instance
+
+            myAlljoyn.Initialize();
+
+            // Register alljoyn interface. We set the 2nd parameter to false to indicate that
+            // properties will not be used
+
+            myAlljoyn.RegisterObjectsInterface(new AJ_Object() { path = ServicePath, interfaces = testInterface }, false, true);
+
+            // Start the message processing loop
+
+            while (true)
+            {
+                if (!connected)
+                {
+                    IntPtr arg = new IntPtr();
+                    status = myAlljoyn.StartService(bus, null, CONNECT_TIMEOUT, AJ.AJ_FALSE, ServicePort, ServiceName, AJ.AJ_NAME_REQ_DO_NOT_QUEUE);
+                    if (status != AJ_Status.AJ_OK)
+                    {
+                        goto exit;
+                    }
+                    myAlljoyn.AlwaysPrintf(("StartService returned AJ_OK; running \n"));
+                    connected = true;
+
+                    if (status == AJ_Status.AJ_OK)
+                    {
+                        myAlljoyn.doAnnounce = true;
+                        myAlljoyn.AboutAnnounce(bus, ServicePort);
+                    }
+                }
+
+                status = myAlljoyn.UnmarshalMsg(bus, msg, UNMARSHAL_TIMEOUT);
+                if (status != AJ_Status.AJ_OK)
+                {
+                    if (status == AJ_Status.AJ_ERR_TIMEOUT)
+                    {
+                        myAlljoyn.AlwaysPrintf(("do work\n"));
+                        goto exit;
+                    }
+                }
+
+                if (status == AJ_Status.AJ_OK)
+                {
+                    string str = "Received message + msgId=" + msg.msgId.ToString("X") + "\n";
+                    myAlljoyn.AlwaysPrintf((str));
+
+                    if (msg.msgId == AJ_METHOD_ACCEPT_SESSION)
+                    {
+                        string joiner = "";
+                        UInt32 sessionId = 0;
+
+                        myAlljoyn.UnmarshalArgs(msg, "qus", port, sessionId, joiner);
+                        status = myAlljoyn.BusReplyAcceptSession(msg, 1);
+
+                        if (status == AJ_Status.AJ_OK)
+                        {
+                            myAlljoyn.AlwaysPrintf(("Accepted session session_id \n"));
+                        }
+                        else
+                        {
+                            myAlljoyn.AlwaysPrintf(("AJ_BusReplyAcceptSession: error \n"));
+                        }
+
+                    }
+                    else if (msg.msgId == AJ_SIGNAL_SESSION_JOINED)
+                    {
+                        // do nothing...
+
+                    }
+                    else if (msg.msgId == BASIC_SERVICE_CAT)
+                    {
+                        status = AppHandleCat(msg, myAlljoyn);
+                    }
+                    else
+                    {
+                        myAlljoyn.BusHandleBusMessage(msg, bus, ServicePort);
+                    }
+                }
+                myAlljoyn.CloseMsg(msg);
+
+                if (status == AJ_Status.AJ_ERR_READ)
+                {
+                    myAlljoyn.AlwaysPrintf(("AllJoyn disconnect\n"));
+                    myAlljoyn.Disconnect(bus);
+                    connected = false;
+
+                    // sleep a little while before trying to connect
+
+                    myAlljoyn.Sleep(10 * 1000);
+                }
+
+            exit:
+                myAlljoyn.AlwaysPrintf((" Exit  \n"));
+            }
+        }
+    }
+}
+
