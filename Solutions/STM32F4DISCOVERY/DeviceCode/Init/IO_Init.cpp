5142cfc0064d8901a2c7dd62fb66bf2b40e4512b
CW2 <CW2@atlas.cz>
2015-07-14 10:26:33 +0200

    STM32F4DISCOVERY: TinyBooter uses ITM0
    USART driver and PAL replaced with stubs.

--- Solutions/STM32F4DISCOVERY/DeviceCode/Init/IO_Init.cpp
+++ Solutions/STM32F4DISCOVERY/DeviceCode/Init/IO_Init.cpp
@@ -14,6 +14,11 @@
 #include <tinyhal.h>
 #include "..\..\..\..\DeviceCode\Targets\Native\STM32F4\DeviceCode\stm32f4xx.h"
 
+// Define the generic port table, only one generic extensionn port type supported
+// and that is the ITM hardware trace port on Channel 0.
+extern GenericPortTableEntry const Itm0GenericPort;
+extern GenericPortTableEntry const* const g_GenericPorts[TOTAL_GENERIC_PORTS] = { &Itm0GenericPort };
+
 extern void STM32F4_GPIO_Pin_Config( GPIO_PIN pin, UINT32 mode, GPIO_RESISTOR resistor, UINT32 alternate );    // Workaround, since CPU_GPIO_DisablePin() does not correctly initialize pin speeds
 
 void __section("SectionForBootstrapOperations") BootstrapCode_GPIO()
