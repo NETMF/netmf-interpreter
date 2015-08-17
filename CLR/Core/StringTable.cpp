fb6a099e3a0c12b8bee6d3df9cb7552f7c878a5a
CW2 <CW2@atlas.cz>
2015-08-17 13:06:42 +0200

    Deleted unused InitString() functions
    TINYCLR_CREATE_STRINGTABLE symbol is not used anymore, never defined.

--- CLR/Core/StringTable.cpp
+++ CLR/Core/StringTable.cpp
@@ -10,57 +10,10 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#if defined(TINYCLR_CREATE_STRINGTABLE)
-#if !defined(BIG_ENDIAN)
-void CLR_RT_Assembly::InitString()
-{
-    NATIVE_PROFILE_CLR_CORE();
-    CLR_STRING* dst    = c_CLR_StringTable_Lookup;
-    LPCSTR      src    = c_CLR_StringTable_Data;
-    size_t      len    = c_CLR_StringTable_Size;
-    size_t      offset = 0;
-
-    while(len-- > 0)
-    {
-        size_t strLen = hal_strlen_s( src ) + 1;
-
-        *dst++ = (CLR_STRING)offset;
-
-        offset += strLen;
-        src    += strLen;
-    }
-}
-#else
-
-void CLR_RT_Assembly::InitString()
-{
-    NATIVE_PROFILE_CLR_CORE();
-    CLR_STRING* dst    = c_CLR_StringTable_Lookup;
-    LPCSTR      src    = c_CLR_StringTable_Data;
-    size_t      len    = c_CLR_StringTable_Size;
-    size_t      offset = 0;
-
-    while(len-- > 0)
-    {
-        size_t strLen = hal_strlen_s( src ) + 1;
-
-        *dst++ = (CLR_STRING)offset;
-
-        offset += strLen;
-        src    += strLen;
-    }
-}
-
-#endif
-
-#else
-
 void CLR_RT_Assembly::InitString()
 {
     NATIVE_PROFILE_CLR_CORE();
 }
-
-#endif
 
 LPCSTR CLR_RT_Assembly::GetString( CLR_STRING i )
 {
