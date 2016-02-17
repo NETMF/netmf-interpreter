d1aebca6d5c19e97adfa9d0bd4b77b3032839424
sjmneves <sergio.neves@eclo.pt>
2016-02-17 19:21:42 +0000

    First pass on adding FreeRTOS

--- Solutions/STM32F4DISCOVERY/DeviceCode/TinyHal/main.cpp
+++ Solutions/STM32F4DISCOVERY/DeviceCode/TinyHal/main.cpp
@@ -13,6 +13,8 @@
 // 
 #include <tinyhal.h>
 //#include "OsHal.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 #ifdef __CC_ARM
 // we include this to error at link time if any of the C semihosted support is imported
@@ -41,10 +43,15 @@ extern osThreadDef_t os_thread_def_main;
 
 #endif
 
+//extern void vTaskStartScheduler( void );
+
 int main(void)
 {
     CPU_GPIO_EnableOutputPin(LED4, TRUE);
 
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+    
         //osKernelInitialize();
         //osThreadCreate(&os_thread_def_main, NULL);
 CPU_GPIO_EnableOutputPin(LED5, TRUE);
@@ -112,4 +119,74 @@ CPU_GPIO_EnableOutputPin(LED5, TRUE);
     CPU_Halt();
 #endif
     return -1;
+}
+
+extern "C" void vApplicationTickHook( void )
+{
+// 	#if ( mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY == 0 )
+// 	{
+// 		/* Just to verify that the interrupt nesting behaves as expected,
+// 		increment ulFPUInterruptNesting on entry, and decrement it on exit. */
+// 		ulFPUInterruptNesting++;
+// 
+// 		/* Fill the FPU registers with 0. */
+// 		vRegTestClearFlopRegistersToParameterValue( 0UL );
+// 
+// 		/* Trigger a timer 2 interrupt, which will fill the registers with a
+// 		different value and itself trigger a timer 3 interrupt.  Note that the
+// 		timers are not actually used.  The timer 2 and 3 interrupt vectors are
+// 		just used for convenience. */
+// 		NVIC_SetPendingIRQ( TIM2_IRQn );
+// 
+// 		/* Ensure that, after returning from the nested interrupts, all the FPU
+// 		registers contain the value to which they were set by the tick hook
+// 		function. */
+// 		configASSERT( ulRegTestCheckFlopRegistersContainParameterValue( 0UL ) );
+// 
+// 		ulFPUInterruptNesting--;
+// 	}
+// 	#endif
+}
+
+extern "C" void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	//taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+extern "C" void vApplicationIdleHook( void )
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+extern "C" void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	//taskDISABLE_INTERRUPTS();
+	for( ;; );
 }
