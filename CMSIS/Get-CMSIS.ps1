569bf982db0aa8a4c0f620090115479fdece8ed3
Steve Maillet <smaillet-ms@users.noreply.github.com>
2016-02-28 10:13:05 -0800

    moved zip stream extraction out to seperate module to allow re-use in other build support scripts

--- CMSIS/Get-CMSIS.ps1
+++ CMSIS/Get-CMSIS.ps1
@@ -8,44 +8,9 @@ Retrieves and extracts the CMSIS pack supported by NETMF from the official GitHu
 Get-CMSIS
 #>
 
-# Function to extract the files contained in a zip file provided as a stream
-Function Extract-PackStream
-{
-    [CmdletBinding()]
-    Param ( [parameter(ValueFromPipeline=$True,Mandatory=$True,ValueFromPipelineByPropertyName=$True)] [System.IO.Stream]$RawContentStream )
-    Begin
-    {
-        [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression") | Out-Null
-        [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-        $rootPath = Get-Location
-    } 
-    Process
-    {
-        $pack = New-Object -TypeName System.IO.Compression.ZipArchive -ArgumentList $RawContentStream
-        $i = 1
-        Foreach( $entry in $pack.Entries )
-        {
-            $targetPath = [System.IO.Path]::Combine( $rootPath, $entry.FullName )
-            $targetDir = [System.IO.Path]::GetDirectoryName( $targetPath )
-            if( $entry.FullName.EndsWith('/') )
-            {
-                if( -not [System.IO.Directory]::Exists( $targetDir ) )
-                {
-                    [System.IO.Directory]::CreateDirectory( $targetDir )
-                }
-            }
-            else
-            {
-                Write-Progress -Activity "Extracting" -Status $entry.FullName -PercentComplete ( $i / $pack.Entries.Count * 100 )
-                [System.IO.Compression.ZipFileExtensions]::ExtractToFile( $entry, $targetPath, $true )
-            }
-            $i = $i + 1
-        }
-        Write-Host "Done Extracting $i files"
-    }
-}
+Import-Module ..\tools\scripts\Build-netmf.psm1
 
-# officially supported version
+# current officially supported version
 $packVersion = "4.3.0"
 
 # FUll versioned pack file name to download
@@ -55,4 +20,4 @@ $packFileName = "ARM.CMSIS.$packVersion.pack"
 $packSourceURLBase = "https://github.com/ARM-software/CMSIS/releases/download/v$packVersion"
 
 # download the pack and extract the files into the curent directory 
-Invoke-WebRequest -Uri "$packSourceURLBase/$packFileName" | Extract-PackStream
+Invoke-WebRequest -Uri "$packSourceURLBase/$packFileName" | Expand-Stream
---
+++ tools/scripts/Build-netmf.psm1
@@ -0,0 +1,56 @@
+﻿<#
+.SYNOPSIS
+
+Function to extract the files contained in a zip file provided as a stream typically from an Invoke-WebRequest
+
+.EXAMPLE
+
+Invoke-WebRequest -Uri "$packSourceURLBase/$packFileName" | Expand-Stream
+#>
+# 
+Function Expand-Stream
+{
+    [CmdletBinding()]
+    Param ( [parameter(ValueFromPipeline=$True,Mandatory=$True,ValueFromPipelineByPropertyName=$True)] [System.IO.Stream]$RawContentStream
+          , [parameter(Mandatory=$False)] [String]$Destination
+          )
+    Begin
+    {
+        [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression") | Out-Null
+        [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+        if( [String]::IsNullOrWhiteSpace( $Destination ) )
+        {
+            $rootPath = Get-Location
+        }
+        else
+        {
+            $rootPath = $Destination
+        }
+    } 
+    Process
+    {
+        $pack = New-Object -TypeName System.IO.Compression.ZipArchive -ArgumentList $RawContentStream
+        $i = 1
+        Foreach( $entry in $pack.Entries )
+        {
+            $targetPath = [System.IO.Path]::Combine( $rootPath, $entry.FullName )
+            $targetDir = [System.IO.Path]::GetDirectoryName( $targetPath )
+            if( $entry.FullName.EndsWith('/') )
+            {
+                if( -not [System.IO.Directory]::Exists( $targetDir ) )
+                {
+                    [System.IO.Directory]::CreateDirectory( $targetDir )
+                }
+            }
+            else
+            {
+                Write-Progress -Activity "Extracting" -Status $entry.FullName -PercentComplete ( $i / $pack.Entries.Count * 100 )
+                [System.IO.Compression.ZipFileExtensions]::ExtractToFile( $entry, $targetPath, $true )
+            }
+            $i = $i + 1
+        }
+        Write-Host "Done Extracting $i files"
+    }
+}
+
+export-modulemember -function Expand-Stream
