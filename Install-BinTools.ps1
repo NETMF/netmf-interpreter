b0fe5cb255676615b9486bfa76802a8d015f7dc5
Steve Maillet <smaillet-ms@users.noreply.github.com>
2016-05-12 20:05:30 -0700

    Added checks for an existing installation to Install-Tools.ps1 and InstallCMSIS.ps1 so that the automated build doesn't need to install the files if they are already available.

--- Install-BinTools.ps1
+++ Install-BinTools.ps1
@@ -1,2 +1,9 @@
 ﻿Import-Module .\tools\scripts\Build-netmf.psm1
+if( Test-Path -PathType Container (Join-Path $SPOROOT "bin\automation") )
+{
+    Write-Host "Existing tools installation found"
+    return
+}
+
 Invoke-WebRequest -UseBasicParsing -Uri "http://netmf.github.io/downloads/build-tools.zip" | Expand-Stream -Destination $SPOROOT
+
